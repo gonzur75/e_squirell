d758a0daf72af9794f891c48a296e0c458750e12
Marcin <gonzur75@gmail.com>
2023-02-28 22:58:35 +0100

    add detatil view test

--- backend/energy_prices/tests/test_energyprice_view.py
+++ backend/energy_prices/tests/test_energyprice_view.py
@@ -11,3 +11,10 @@ def test_energy_price_listview(client, energy_price):
     assert response.status_code == 200
     assert str(energy_price.value_inc_vat) in response.content.decode('utf-8')
     assert EnergyPrice.objects.count() == 1
+
+def test_energy_price_detail_view(client, energy_price):
+    endpoint = reverse("energy_price_api:energy_price_detail", kwargs={"pk": energy_price.valid_from})
+    response = client.get(endpoint, format="json")
+
+    assert response.status_code == 200
+    assert str(energy_price.value_inc_vat) in response.content.decode('utf-8')
