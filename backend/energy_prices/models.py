2911a4ebe27555c6cbf36a7b79e716c6976480d9
Marcin <gonzur75@gmail.com>
2023-02-28 23:15:26 +0100

    corrected model error

--- backend/energy_prices/models.py
+++ backend/energy_prices/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class EnergyPrice(models.Model):
-    valid_from = models.DateTimeField(primary_key=True, editable=False, help_text='Time at which given price starts')
+    valid_from = models.DateTimeField(editable=False, help_text='Time at which given price starts')
     valid_to = models.DateTimeField(editable=False, help_text='Time at which given price stops')
     value_inc_vat = models.DecimalField(max_digits=4,
                                         decimal_places=2,
