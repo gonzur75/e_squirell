03c24925e6bcd7eb70ed2becb625b076c888af99
Marcin <gonzur75@gmail.com>
2023-03-02 20:00:26 +0100

    refactor view.py

--- backend/energy_prices/views.py
+++ backend/energy_prices/views.py
@@ -1,18 +1,11 @@
-from django.shortcuts import render
 from rest_framework import generics, viewsets
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from . import models, serializers
 from .serializers import EnergyPriceSerializer
 
 
-# Create your views here.
 class EnergyPriceViewSet(viewsets.ModelViewSet):
     queryset = models.EnergyPrice.objects.all()
     serializer_class = EnergyPriceSerializer
     # permission_classes = [IsAuthenticatedOrReadOnly]
 
-
-class DetailEnergyPrice(generics.RetrieveAPIView):
-    queryset = models.EnergyPrice.objects.all()
-    serializer_class = serializers.EnergyPriceSerializer
