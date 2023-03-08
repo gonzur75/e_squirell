968f542104e39de861af49839e5dd7d338b64747
Marcin <gonzur75@gmail.com>
2023-03-08 21:35:26 +0100

    max_digits increased to 8, and decimal_places increased to 3 in EnergyPrice model value_inc_vat field

--- backend/energy_prices/models.py
+++ backend/energy_prices/models.py
@@ -4,8 +4,8 @@ from django.db import models
 class EnergyPrice(models.Model):
     valid_from = models.DateTimeField(help_text='Time at which given price starts')
     valid_to = models.DateTimeField(help_text='Time at which given price stops')
-    value_inc_vat = models.DecimalField(max_digits=4,
-                                        decimal_places=2,
+    value_inc_vat = models.DecimalField(max_digits=8,
+                                        decimal_places=3,
                                         help_text='energy price per kwh, with Vat',
                                         verbose_name='Price per kwh'
                                         )
