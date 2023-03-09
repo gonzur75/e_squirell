12982a83ef0b1a4b374947f18f8f86e4df306ae8
Marcin <gonzur75@gmail.com>
2023-03-09 22:03:25 +0100

    add unique=TRUE to valid_from, valid_to fields

--- backend/energy_prices/models.py
+++ backend/energy_prices/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class EnergyPrice(models.Model):
-    valid_from = models.DateTimeField(help_text='Time at which given price starts')
-    valid_to = models.DateTimeField(help_text='Time at which given price stops')
+    valid_from = models.DateTimeField(help_text='Time at which given price starts', unique=True)
+    valid_to = models.DateTimeField(help_text='Time at which given price stops', unique=True)
     value_inc_vat = models.DecimalField(max_digits=8,
                                         decimal_places=3,
                                         help_text='energy price per kwh, with Vat',
--- backend/energy_prices/views.py
+++ backend/energy_prices/views.py
@@ -1,12 +1,23 @@
 from rest_framework import generics, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from . import models
+from .models import EnergyPrice
 from .serializers import EnergyPriceSerializer
 
 
 class EnergyPriceViewSet(viewsets.ModelViewSet):
     queryset = models.EnergyPrice.objects.all()
     serializer_class = EnergyPriceSerializer
+
     # permission_classes = [IsAuthenticatedOrReadOnly]
+    @action(detail=False)
+    def last_entry(self, request):
+        last_entry = EnergyPrice.objects.latest('valid_from')
+        print(last_entry)
+        serializer = self.get_serializer(last_entry)
+        return Response(serializer.data)
+
 
 # TODO: decide weather you want authentication or not
