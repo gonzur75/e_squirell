1c4c9b190c15f62a72c2bee4ea8b169c2b8bfb6b
Marcin Włodarczyk <gonzur75@gmail.com>
2024-07-24 12:52:42 +0200

    refactor wifi connection into a function with messages and error handling

--- esp32_soft/boot.py
+++ esp32_soft/boot.py
@@ -1,19 +1,16 @@
-# Complete project details at https://RandomNerdTutorials.com
 
 import time
-from umqttsimple import MQTTClient
 import ubinascii
 import machine
-import micropython
 import network
 import esp
+
 esp.osdebug(None)
 import gc
+
 gc.collect()
 
-ssid = "Strugalowka"  # 'REPLACE_WITH_YOUR_SSID'
-password = "Akacja17"  # 'REPLACE_WITH_YOUR_PASSWORD'
-mqtt_server = "192.168.8.2"   # 'REPLACE_WITH_YOUR_MQTT_BROKER_IP'
+mqtt_server = "192.168.8.2"  # 'REPLACE_WITH_YOUR_MQTT_BROKER_IP'
 # EXAMPLE IP ADDRESS
 # mqtt_server = '172.17.0.1'
 client_id = ubinascii.hexlify(machine.unique_id())
@@ -23,15 +20,39 @@ topic_pub = b'hello'
 last_message = 0
 message_interval = 5
 counter = 0
+SSID = 'Strugalowka'
+PASSWORD = 'Akacja17'
 
 
-station = network.WLAN(network.STA_IF)
+def wlan_connect(ssid, password, max_retries=200):
+    wlan = network.WLAN(network.STA_IF)
+    try:
+        print(f'Connecting to network: {ssid}.')
+        wlan.active(True)
+        wlan.connect(ssid, password)
 
-station.active(True)
-station.connect(ssid, password)
+        for _ in range(max_retries):
+            if wlan.isconnected():
+                print(f'\nConnected. \nNetwork config: {wlan.ifconfig()}')
+                return True
+            time.sleep(0.1)
+            print('.', end='')
 
-while station.isconnected() == False:
-  print('disconnected')
+        print(f'Failed to connect to {ssid}')
 
-print('Connection successful')
-print(station.ifconfig())
+    except OSError as error:
+        print(f'System returned error with message:{error}. \nPlease restart your device ')
+
+
+wlan_connect(SSID, PASSWORD)
+
+# station = network.WLAN(network.STA_IF)
+#
+# station.active(True)
+# station.connect(ssid, password)
+#
+# while station.isconnected() == False:
+#   print('disconnected')
+#
+# print('Connection successful')
+# print(station.ifconfig())
