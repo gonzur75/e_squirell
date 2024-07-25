5f77ae53609ba98aac53058b382e4caa115b3fc3
Marcin Włodarczyk <gonzur75@gmail.com>
2024-07-25 12:01:33 +0200

    working code that allow for sending and reciving messages

--- esp32_soft/main.py
+++ esp32_soft/main.py
@@ -1,6 +1,5 @@
 # Complete project details at https://RandomNerdTutorials.com
 import time
-import micropython
 import ubinascii
 
 import machine
@@ -10,22 +9,21 @@ from umqttsimple import MQTTClient
 
 MQTT_SERVER = "192.168.8.2"  # 'YOUR_MQTT_BROKER_IP'
 TOPIC = b'heat_storage'
-cllient_id = ubinascii.hexlify(machine.unique_id())
+
 
 def sub_cb(topic, msg):
-    print((topic, msg))
+    print(topic, msg)
     if msg == b'get_temps':
-        print('Sending temps received hello message')
+        print('Sending temps')
 
 
 def connect_and_subscribe(topic_sub, mqtt_server, client_id=None):
     client_id = client_id if client_id is not None else ubinascii.hexlify(machine.unique_id())
 
     _client = MQTTClient(client_id, mqtt_server)
-
     _client.set_callback(sub_cb)
     _client.connect()
-    _client.subscribe(b'heat_storage_services')
+    _client.subscribe(TOPIC)
     print('Connected to %s MQTT broker, subscribed to %s topic' % (mqtt_server, topic_sub))
     return _client
 
@@ -46,14 +44,13 @@ def read_temperature():
 
 
 try:
-    client = connect_and_subscribe(TOPIC, MQTT_SERVER, cllient_id)
+    client = connect_and_subscribe(TOPIC, MQTT_SERVER)
 except OSError as e:
     restart_and_reconnect(e)
 
 
 def run(message_interval=5):
     last_message = 0
-    counter = 0
 
     def run_instance_():
         nonlocal last_message
