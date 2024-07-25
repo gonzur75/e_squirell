b31d93c709309f85040a601cdb25d0ccf49c51aa
Marcin Włodarczyk <gonzur75@gmail.com>
2024-07-25 11:12:51 +0200

    fixed noot working tennary operator, add run function with closure

--- esp32_soft/main.py
+++ esp32_soft/main.py
@@ -10,18 +10,19 @@ from umqttsimple import MQTTClient
 
 MQTT_SERVER = "192.168.8.2"  # 'YOUR_MQTT_BROKER_IP'
 TOPIC = b'heat_storage'
-
+cllient_id = ubinascii.hexlify(machine.unique_id())
 
 def sub_cb(topic, msg):
     print((topic, msg))
-    if topic == b'notification' and msg == b'received':
-        print('ESP received hello message')
+    if msg == b'get_temps':
+        print('Sending temps received hello message')
 
 
 def connect_and_subscribe(topic_sub, mqtt_server, client_id=None):
-    client_id = client_id if not None else ubinascii.hexlify(machine.unique_id())
+    client_id = client_id if client_id is not None else ubinascii.hexlify(machine.unique_id())
 
     _client = MQTTClient(client_id, mqtt_server)
+
     _client.set_callback(sub_cb)
     _client.connect()
     _client.subscribe(b'heat_storage_services')
@@ -42,23 +43,39 @@ def read_temperature():
     readings = ds.scan()
     ds.convert_temp()
     time.sleep_ms(750)
-    for read in readings:
-        print(ds.read_temp(read))
 
 
 try:
-    client = connect_and_subscribe(TOPIC, MQTT_SERVER)
+    client = connect_and_subscribe(TOPIC, MQTT_SERVER, cllient_id)
 except OSError as e:
     restart_and_reconnect(e)
 
-while True:
 
-    try:
-        client.check_msg()
-        if (time.time() - last_message) > message_interval:
-            msg = b'Hello #%d' % counter
-            client.publish(topic_pub, msg)
-            last_message = time.time()
-            counter += 1
-    except OSError as e:
-        restart_and_reconnect(e)
+def run(message_interval=5):
+    last_message = 0
+    counter = 0
+
+    def run_instance_():
+        nonlocal last_message
+
+        try:
+            client.check_msg()
+
+            if (time.time() - last_message) > message_interval:
+                msg = b'Hello'
+                print(f'sending messsage')
+                client.publish(TOPIC, msg)
+                last_message = time.time()
+
+        except OSError as e:
+            restart_and_reconnect(e)
+
+        return
+
+    return run_instance_
+
+
+run_instance = run()
+
+while True:
+    run_instance()
