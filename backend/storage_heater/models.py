250bdd83fa9326d772b78a5faf01aed15e28dee9
Marcin Włodarczyk <gonzur75@gmail.com>
2024-08-07 14:36:50 +0200

    add storage_heater model to record status of storage heater

--- backend/storage_heater/models.py
+++ backend/storage_heater/models.py
@@ -1,3 +1,21 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
+
 # Create your models here.
+class StorageHeater(models.Model):
+    time_stamp = models.DateTimeField(help_text='Time of taking record', primary_key=True, unique=True,
+                                      auto_now_add=True)
+    relay_one = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(1)])
+    relay_two = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(1)])
+    relay_three = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(1)])
+    relay_four = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(1)])
+    relay_five = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(1)])
+    relay_six = models.IntegerField(default=0, validators=[MinValueValidator(0), MaxValueValidator(1)])
+    temp_one = models.FloatField(default=0.0, validators=[MinValueValidator(0.0), MaxValueValidator(120.0)])
+    temp_two = models.FloatField(default=0.0, validators=[MinValueValidator(0.0), MaxValueValidator(120.0)])
+    temp_three = models.FloatField(default=0.0, validators=[MinValueValidator(0.0), MaxValueValidator(120.0)])
+    temp_four = models.FloatField(default=0.0, validators=[MinValueValidator(0.0), MaxValueValidator(120.0)])
+
+    def __str__(self):
+        return f'Storage heater status at {self.time_stamp}'
--- backend/storage_heater/tests/test_model_storage_heater.py
+++ backend/storage_heater/tests/test_model_storage_heater.py
@@ -1,24 +1,36 @@
-from backend.conftest import fake
+import pytest
+
+from conftest import fake
+
+from storage_heater.models import StorageHeater
 
 
+@pytest.fixture
 def storage_heater_status(db):
-    return StorageHeater.objects.create(
-        time_stamp=fake.date_time_this_month(),
+    yield StorageHeater.objects.create(
         relay_one=fake.random_int(min=0, max=1),
         relay_two=fake.random_int(min=0, max=1),
         relay_three=fake.random_int(min=0, max=1),
         relay_four=fake.random_int(min=0, max=1),
         relay_five=fake.random_int(min=0, max=1),
         relay_six=fake.random_int(min=0, max=1),
-        temp_one=fake.pyflaot(left_digits=1, right_digits=2,min_value=-55, max_value=120),
-        temp_two=fake.pyflaot(left_digits=1, right_digits=2,min_value=-55, max_value=120),
-        temp_three=fake.pyflaot(left_digits=1, right_digits=2,min_value=-55, max_value=120),
-        temp_four=fake.pyflaot(left_digits=1, right_digits=2,min_value=-55, max_value=120),
-
+        temp_one=fake.pyfloat(left_digits=3, right_digits=1, min_value=-55, max_value=120),
+        temp_two=fake.pyfloat(left_digits=3, right_digits=1, min_value=-55, max_value=120),
+        temp_three=fake.pyfloat(left_digits=3, right_digits=1, min_value=-55, max_value=120),
+        temp_four=fake.pyfloat(left_digits=3, right_digits=1, min_value=-55, max_value=120),
     )
 
 
-def test_storage_heater_status_creation():
-    assert isinstance(storage_heater, StorageHeater)
+def test_storage_heater_status_creation(storage_heater_status):
+    assert isinstance(storage_heater_status, StorageHeater)
+    assert [*vars(storage_heater_status)] == [
+        '_state', 'time_stamp', 'relay_one', 'relay_two', 'relay_three', 'relay_four',
+        'relay_five', 'relay_six', 'temp_one', 'temp_two', 'temp_three',
+        'temp_four']
 
+    for var in vars(storage_heater_status):
+        if var.startswith('relay'):
+            assert getattr(storage_heater_status, var) in (0, 1)
 
+        if var.startswith('temp'):
+            assert isinstance(getattr(storage_heater_status, var), float)
