c67abd3b8b4247d41e40f4227b216a83f661117e
Marcin Włodarczyk <gonzur75@gmail.com>
2024-11-13 14:28:37 +0100

    ba

--- backend/energy_prices/urls.py
+++ backend/energy_prices/urls.py
@@ -1,4 +1,3 @@
-from django.urls import path
 from rest_framework.routers import SimpleRouter
 
 from . import views
@@ -9,8 +8,3 @@ router = SimpleRouter()
 router.register('energy_prices', views.EnergyPriceViewSet, basename='energy_prices')
 
 urlpatterns = router.urls
-# urlpatterns = [
-#
-#     # path('', views.ListEnergyPrice.as_view(), name="energy_price_list"),
-#     # path('<int:pk>/', views.DetailEnergyPrice.as_view(), name="energy_price_detail"),
-# ]
--- backend/storage_heater/views.py
+++ backend/storage_heater/views.py
@@ -3,7 +3,6 @@ from storage_heater.models import StorageHeater
 from storage_heater.serializers import StorageHeaterSerializer
 
 
-# Create your views here.
 class HeatStorageViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = StorageHeater.objects.all()
     serializer_class = StorageHeaterSerializer
