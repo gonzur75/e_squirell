a8bbef9ed6e54431d59bd3a4bfc4cac443a9777f
Marcin Włodarczyk <gonzur75@gmail.com>
2025-05-06 15:41:06 +0200

    add pagination

--- backend/energy_tracker/views.py
+++ backend/energy_tracker/views.py
@@ -1,10 +1,16 @@
-
 from rest_framework import viewsets
-
+from rest_framework.pagination import PageNumberPagination
 from energy_tracker.models import EnergyLog
 from energy_tracker.serializers import EnergyLogSerializer
+
+
+class StandardResultsSetPagination(PageNumberPagination):
+    page_size = 100
+    page_size_query_param = 'page_size'
+    max_page_size = 1000
 
 
 class EnergyLogViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = EnergyLog.objects.all()
     serializer_class = EnergyLogSerializer
+    pagination_class = StandardResultsSetPagination
