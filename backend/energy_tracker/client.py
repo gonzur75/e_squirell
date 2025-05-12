0d8a508c4d9dc7f2d01ee5f6880d6f51ab9a2b81
Marcin Włodarczyk <gonzur75@gmail.com>
2025-05-12 14:20:13 +0200

    add logic to stabilise voltage first version

--- backend/energy_tracker/client.py
+++ backend/energy_tracker/client.py
@@ -1,15 +1,22 @@
 import logging
 import json
 import os
+from datetime import datetime
 from pathlib import Path
 from typing import Dict, Any, Optional
 
+import pytz
 import tinytuya
-
+from django.conf import settings
 
 from energy_tracker.serializers import EnergyLogSerializer
+import paho.mqtt.publish as publish
+
+from storage_heater.models import StorageHeater
 
 logger = logging.getLogger(__name__)
+
+HEATING_REQUIRED = False
 
 
 class PC321MeterClient:
@@ -74,7 +81,7 @@ class PC321MeterClient:
         return processed_data
 
     @staticmethod
-    def save_reading_to_db(data: Dict[str, Any]) -> None:
+    def save_reading_to_db(data: Dict[str, Any]) -> dict | None:
         """
         Save the processed data to the Django model.
         Args:
@@ -85,13 +92,15 @@ class PC321MeterClient:
             serializer = EnergyLogSerializer(data=data)
             if serializer.is_valid(raise_exception=True):
                 serializer.save()
+                return serializer.validated_data
+            return None
 
         except Exception as e:
             print(e)
 
     # logger.exception(f"Failed to save reading to database: {e}")
 
-    def fetch_and_save(self) -> bool:
+    def fetch_and_process_data(self) -> bool:
         """
         Fetch data from the Tuya device and save it to the database.
 
@@ -104,8 +113,8 @@ class PC321MeterClient:
                 return False
 
             processed_data = self.process_device_data(status_data)
-            if processed_data:
-                self.save_reading_to_db(processed_data)
+            if processed_data and (validated_data := self.save_reading_to_db(processed_data)):
+                process_smart_meter_data(validated_data)
                 return True
             else:
                 logger.warning("No data was processed from device status")
@@ -113,3 +122,93 @@ class PC321MeterClient:
         except Exception as e:
             logger.exception(f"Error in fetch_and_save: {e}")
             return False
+
+
+RELAYS_MAP = {
+    "a": 1,
+    "b": 2,
+    "c": 3,
+}
+
+
+def process_smart_meter_data(data: dict):
+    active_power = data["total_active_power"]
+    voltage_threshold = 2510
+    safe_voltage_threshold = 2480
+    # [('voltage_a', 2134),  ...]
+    voltage_per_phase = sorted((x for x in data.items() if x[0] in ('voltage_a', 'voltage_b', 'voltage_c')),
+                               key=lambda x: x[1])
+    if HEATING_REQUIRED:
+
+        if active_power > 2400:
+            for phase_name, _ in voltage_per_phase[:3]:
+                send_relay_action(phase_name, 'on')
+        elif active_power > 1600:
+            for phase_name, _ in voltage_per_phase[:2]:
+                send_relay_action(phase_name, 'on')
+            phase_name, _ = voltage_per_phase[2]
+            send_relay_action(phase_name, 'off')
+        elif active_power > 800:
+            for phase_name, _ in voltage_per_phase[1:]:
+                send_relay_action(phase_name, 'off')
+            phase_name, _ = voltage_per_phase[0]
+            send_relay_action(phase_name, 'on')
+        else:
+            for phase_name, _ in voltage_per_phase:
+                send_relay_action(phase_name, 'off')
+
+    elif 10 <= datetime.now(pytz.timezone('Europe/Warsaw')).hour < 16:
+        # stabilising voltage in case of high energy production in sumer when heating is not required
+        # but it could be used for droping excesiv voltage that coses falownik to switchoff
+        relays: list[str] = [x.name for x in StorageHeater._meta.get_fields() if x.name.startswith('relay_')]
+        storage_heater_relays = StorageHeater.objects.values(*relays).first()
+        one_kw_heaters_on = {k: v for k, v in list(storage_heater_relays.items())[:3] if v is True}
+        two_kw_heaters_on = {k: v for k, v in list(storage_heater_relays.items())[3:] if v is True}
+
+        phases_with_voltage_over_threshold = [x for x in voltage_per_phase if x[1] > voltage_threshold]
+        phases_with_voltage_under_safe_threshold = all([x for x in voltage_per_phase if x[1] < safe_voltage_threshold])
+
+        if phases_with_voltage_over_threshold:
+            if one_kw_heaters_on and not two_kw_heaters_on:
+                match_one_kw_heaters_with_overvoltage_phases = []
+                for heater in one_kw_heaters_on:
+                    for phase, _ in phases_with_voltage_over_threshold:
+                        phase_number = get_relay_number(phase)
+                        if f"relay_{phase_number}" in heater:
+                            match_one_kw_heaters_with_overvoltage_phases.append(phase_number)
+                            break
+                if len(match_one_kw_heaters_with_overvoltage_phases) == 1:
+                    phase_number = match_one_kw_heaters_with_overvoltage_phases[0]
+                    send_relay_action(phase_number + 3, 'on')
+                    send_relay_action(phase_number, 'off')
+
+                elif not match_one_kw_heaters_with_overvoltage_phases:
+                    for phase, voltage in phases_with_voltage_over_threshold:
+                        relay_number = get_relay_number(phase)
+                        send_relay_action(relay_number, 'on')
+
+                elif len(match_one_kw_heaters_with_overvoltage_phases) > 1:
+                    turn_off_relays(voltage_per_phase)
+
+            elif one_kw_heaters_on and two_kw_heaters_on:
+                turn_off_relays(voltage_per_phase)
+
+            else:
+                for phase, voltage in phases_with_voltage_over_threshold:
+                    relay_number = get_relay_number(phase)
+                    send_relay_action(relay_number, 'on')
+
+        elif phases_with_voltage_under_safe_threshold:
+            turn_off_relays(voltage_per_phase)
+
+def send_relay_action(relay_number, relay_action="on"):
+    payload = {"heating_action": f'relay_{relay_number}_{relay_action}'}
+    publish.single(settings.MQTT_TOPIC, json.dumps(payload), hostname=settings.MQTT_SERVER)
+
+
+def get_relay_number(phase_name):
+    return RELAYS_MAP[phase_name[-1]]
+
+def turn_off_relays(phases):
+    for phase, _ in phases:
+        send_relay_action(get_relay_number(phase), 'off')
--- backend/energy_tracker/tasks.py
+++ backend/energy_tracker/tasks.py
@@ -5,20 +5,24 @@ from energy_tracker.client import PC321MeterClient
 
 logger = logging.getLogger(__name__)
 
+
 @shared_task
 def fetch_and_save_energy_data():
     """
     Celery task to fetch data from Tuya smart meter and save to database.
     This task can be scheduled to run periodically using Celery Beat.
     """
+
+    
     try:
+
         device = settings.SMART_METER
-        
         client = PC321MeterClient(**device)
-        
-        success = client.fetch_and_save()
+        success = client.fetch_and_process_data()
+
         if success:
             return True
+
         else:
             logger.error(f"Failed to fetch data for device {device['device_id']}")
             return False
