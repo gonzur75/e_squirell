243b625f53b0a628c66a8450c9def73d72c70796
Marcin Włodarczyk <gonzur75@gmail.com>
2025-05-13 15:50:26 +0200

    some fixes

--- backend/config/asgi.py
+++ backend/config/asgi.py
@@ -13,6 +13,5 @@ import django
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()
 
 application = get_asgi_application()
